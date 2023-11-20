80cb76d488fee5f4517c16a53bbf41b7abd76bfb
Tom Fyfe <tomfyfe85@gmail.com>
2023-11-20 14:08:10 +0000

    board.js -added handleClick() function

---
+++ src/components/Board.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import Square from "./Square";
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    console.log(nextSquares)
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
